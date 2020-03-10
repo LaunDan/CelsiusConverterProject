1ebb4682ef19b27c15de8652367212a83e28ec22
LaunDan <launadaniel2@gmail.com>
2020-03-10 19:51:11 +0100

    set celsius converter

--- src/CelsiusConverterGUI.java
+++ src/CelsiusConverterGUI.java
@@ -1,17 +1,30 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class CelsiusConverterGUI extends JFrame{
+public class CelsiusConverterGUI extends JFrame {
     private JPanel mainPanel;
+    private JTextField celsiusTextField;
+    private JLabel celsiusLabel;
+    private JButton convertButton;
+    private JLabel farhenheitLabel;
 
-    public CelsiusConverterGUI(String title){
+    public CelsiusConverterGUI(String title) {
         super(title);
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(mainPanel);
         this.pack();
+        convertButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int tempFarh = (int) ((Double.parseDouble(celsiusTextField.getText())) * 1.8 + 32);
+                farhenheitLabel.setText(tempFarh + " Farhenheit");
+            }
+        });
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         JFrame frame = new CelsiusConverterGUI("My celsius converter");
         frame.setVisible(true);
     }
